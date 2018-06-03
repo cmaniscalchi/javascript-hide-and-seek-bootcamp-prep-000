ff5210762fa4002d56914cf60fde6d9a0e879417
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-06-03 19:06:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function getFirstSelector(selector) {
+
+var rankedList = document.getElementsByClassName('ranked-list').document.querySelector('ranked-list');
+
+var divs = document.getElementById('nested');
+  
+  return rankedList, divs; 
+}
