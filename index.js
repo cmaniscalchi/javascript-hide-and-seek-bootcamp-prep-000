81a2f71331d98802629b46abf9158779904f98e9
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-06-03 19:41:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getFirstSelector(selector) {
 
-var rankedList = document.querySelector('ranked-list');
+var rankedList = document.querySelector('ul.ranked-list li');
 
 var divs = document.getElementById('nested');
   
