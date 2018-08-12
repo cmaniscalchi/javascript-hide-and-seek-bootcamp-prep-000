9ca3aca173521d7e314bcba4d59d4dd0dd6240d2
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-08-12 19:54:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 function getFirstSelector(selector) {
-  return document.querySelector(selector); 
+  return document.querySelector(selector)
 }
 
 function nestedTarget() {
-  return document.querySelector('#nested .target');
+  return document.querySelector('#nested .target')
 }
 
 function deepestChild() {
-  return document.querySelector('#grand-node div div div div');
+  return document.querySelector('#grand-node div div div div')
 }
 
 function increaseRankBy(n) {
- var rankedLists = document.querySelectorAll('.ranked-list');
- 
- for (let i = 0; i < rankedLists.length; i++) {
-   rankedLists[i].innerHTML = (n + i).toString();
- }
- return rankedLists;
+  let lis = document.querySelectorAll('.ranked-list li')
+  for (let li of lis) {
+    li.innerHTML = (parseInt(li.innerHTML) + n).toString()
+  }
+  return lis
 }
